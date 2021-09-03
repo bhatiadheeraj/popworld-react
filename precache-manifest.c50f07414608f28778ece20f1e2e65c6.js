2444c46fca6460565c4eda06e63d1589eed38894
Dheeraj Bhatia <dheerajb@buffalo.edu>
2021-09-03 21:40:41 +0000

    Updates

---
+++ precache-manifest.c50f07414608f28778ece20f1e2e65c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1905a678a2395d3819be",
+    "url": "/pop-world/static/css/main.c61a6f3b.chunk.css"
+  },
+  {
+    "revision": "1905a678a2395d3819be",
+    "url": "/pop-world/static/js/main.1905a678.chunk.js"
+  },
+  {
+    "revision": "3c3354a8772a3a7eaea3",
+    "url": "/pop-world/static/js/runtime~main.3c3354a8.js"
+  },
+  {
+    "revision": "685b17926f51fbd3300b",
+    "url": "/pop-world/static/css/2.ae0ece87.chunk.css"
+  },
+  {
+    "revision": "685b17926f51fbd3300b",
+    "url": "/pop-world/static/js/2.685b1792.chunk.js"
+  },
+  {
+    "revision": "ab17526e321b9900097d4e2438985b71",
+    "url": "/pop-world/static/media/Landing_Page_Image.ab17526e.jpg"
+  },
+  {
+    "revision": "b3af730bf6232eb660ebbd3cfdb87ee6",
+    "url": "/pop-world/index.html"
+  }
+];
